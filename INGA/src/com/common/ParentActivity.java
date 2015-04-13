2a1131752d6f4d5af0b9912ff39576fbddce1fab
Manpreeet <mca.33manpreet@gmail.com>
2015-04-13 20:00:05 +0530

    Update ParentActivity.java

--- INGA/src/com/common/ParentActivity.java
+++ INGA/src/com/common/ParentActivity.java
@@ -8,6 +8,7 @@ import android.view.Window;
 
 public class ParentActivity extends Activity {
 	GoogleCloudMessaging cloudMessaging;  
+	public static CustomProgressDialog customProgressDialog;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -16,14 +17,60 @@ public class ParentActivity extends Activity {
 	}
 
 	@Override
-	public void onBackPressed() {
-		super.onBackPressed();
-	}
-
-	@Override
 	protected void onStart() {
 		super.onStart();
 	}
+	@Override
+	public void onBackPressed() {
+		super.onBackPressed();
+
+		try {
+			if (customProgressDialog != null) {
+				customProgressDialog.setCancelable(true);
+				customProgressDialog.dismiss();
+				customProgressDialog = null;
+			}
+		} catch (Exception e) {
+
+		}
+	}
+
+	public void showProgressBar() {
+		try {
+			Log.e("Inside Progress dialog", "wow");
+			if (customProgressDialog == null) {
+				customProgressDialog = new CustomProgressDialog(this);
+				customProgressDialog.show();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void showProgressBar(Boolean isCancelable) {
+		try {
+			if (customProgressDialog == null) {
+				customProgressDialog = new CustomProgressDialog(this);
+				customProgressDialog.show();
+				customProgressDialog.setCancelable(isCancelable);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void dismissProgressBar() {
+		try {
+			if (customProgressDialog != null) {
+				customProgressDialog.setCancelable(true);
+				customProgressDialog.dismiss();
+				customProgressDialog = null;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 
 	
 }
