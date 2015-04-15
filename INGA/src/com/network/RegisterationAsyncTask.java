5ec5692f0ef89780607d389db906b0551c7c0397
Manpreeet <mca.33manpreet@gmail.com>
2015-04-15 11:22:37 +0530

    Update RegisterationAsyncTask.java

--- INGA/src/com/network/RegisterationAsyncTask.java
+++ INGA/src/com/network/RegisterationAsyncTask.java
@@ -13,78 +13,80 @@ import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONObject;
 
+import android.content.Context;
+import android.os.AsyncTask;
+
 import com.common.AppConstant;
 import com.common.ParentActivity;
 
-import android.content.Context;
-import android.os.AsyncTask;
-import android.util.Log;
-
 public class RegisterationAsyncTask extends AsyncTask<Void, Void, Void> {
-	String email, name, firstname, lastname, password, url, device_token,
-			response;
+	private String url = null, username = null, password = null, email = null,
+			firstname = null, lastname = null, device_token = null;
 	private Context context;
 	private ParentActivity activity;
+	private String response;
 
-	public RegisterationAsyncTask(Context context,
-			ParentActivity parentActivity, String url, String email,
-			String username, String password, String first_name,
-			String last_name, String deviceToken) {
-		this.context = context;
-		this.url = url;
-		this.name = username;
+	public RegisterationAsyncTask(Context context,ParentActivity parentactivity, String _url,
+			String username, String email, String password, String first_name,
+			String last_name, String device_token) {
+		this.username = username;
 		this.email = email;
 		this.password = password;
 		this.firstname = first_name;
-		this.lastname = last_name;
-		this.device_token = deviceToken;
-		activity = parentActivity;
+		this.lastname = device_token;
+		this.device_token = username;
+
+		this.url = _url;
+		this.context = context;
+		this.activity = parentactivity;
 	}
 
 	@Override
 	protected void onPreExecute() {
 		super.onPreExecute();
-		// activity.showProgressBar();
+		activity.showProgressBar();
+
 	}
 
 	@Override
 	protected Void doInBackground(Void... params) {
+
 		HttpClient client = new DefaultHttpClient();
 		HttpPost httpPost = new HttpPost(url);
 		try {
 			List<NameValuePair> list = new ArrayList<NameValuePair>();
-			list.add(new BasicNameValuePair(AppConstant.username, name));
+			list.add(new BasicNameValuePair(AppConstant.username, username));
 			list.add(new BasicNameValuePair(AppConstant.email, email));
 			list.add(new BasicNameValuePair(AppConstant.password, password));
-			list.add(new BasicNameValuePair(AppConstant.firstName, firstname));
-			list.add(new BasicNameValuePair(AppConstant.lastName, lastname));
-			list.add(new BasicNameValuePair(AppConstant.deviceToken,
+			list.add(new BasicNameValuePair(AppConstant.first_name, firstname));
+			list.add(new BasicNameValuePair(AppConstant.last_name, lastname));
+			list.add(new BasicNameValuePair(AppConstant.device_token,
 					device_token));
 			httpPost.setEntity(new UrlEncodedFormEntity(list));
-			HttpResponse httpResponse = client.execute(httpPost);
-			response = EntityUtils.toString(httpResponse.getEntity());
-			Log.e("TAG RESPONSE", response);
+			HttpResponse httpResponse=client.execute(httpPost);
+			response=EntityUtils.toString(httpResponse.getEntity());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
 		return null;
-
 	}
 
 	@Override
 	protected void onPostExecute(Void result) {
 		super.onPostExecute(result);
-		// activity.dismissProgressBar();
+		activity.dismissProgressBar();
 		try {
 			JSONObject jsonObject = new JSONObject(response);
 			String error = jsonObject.getString(AppConstant.error);
-			if (error.equals("0")) {
-				activity.onResponseRecieve(jsonObject
-						.getString(AppConstant.msg));
-			} else if (error.equals("1")) {
-				activity.onErrorRecieve(jsonObject.getString(AppConstant.msg));
+			String msg = jsonObject.getString(AppConstant.message);
+			if (error.equals(AppConstant.errorCode)) {
+				activity.onResponseRecieve(msg);
+
+			} else {
+				activity.onErrorRecieve(msg);
 			}
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
