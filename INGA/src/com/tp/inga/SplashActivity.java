f2840dee04361cc27130c1a93a57e42d044a6643
Manpreeet <mca.33manpreet@gmail.com>
2015-04-15 11:21:23 +0530

    Update SplashActivity.java

--- INGA/src/com/tp/inga/SplashActivity.java
+++ INGA/src/com/tp/inga/SplashActivity.java
@@ -1,38 +1,51 @@
 package com.tp.inga;
 
+import com.common.Utilites;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Window;
 
-
 public class SplashActivity extends Activity {
 	Thread thread;
+	private Utilites utilites;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_splash);
-        thread = new Thread(new Runnable() {
-			
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.activity_splash);
+		finId();
+		thread = new Thread(new Runnable() {
+
 			@Override
 			public void run() {
 				// TODO Auto-generated method stub
 				try {
-					thread.sleep(2*1000);
-					 Intent intent= new Intent(SplashActivity.this,HomeActivity.class);
-					 startActivity(intent);
-					 finish();
+					thread.sleep(2 * 1000);
+					Intent intent = new Intent(SplashActivity.this,
+							HomeActivity.class);
+					startActivity(intent);
+					finish();
 				} catch (InterruptedException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}
 		});
-        thread.start();
-       
-    }
+		thread.start();
 
+	}
+
+	private void finId() {
+		utilites = new Utilites(SplashActivity.this);
+		if (utilites.getLoggedId()) {
+			Utilites.setIntent(SplashActivity.this, HomeActivity.class, true);
+		} else {
+			Utilites.setIntent(SplashActivity.this, LoginActivity.class, true);
+
+		}
+	}
 
 }
